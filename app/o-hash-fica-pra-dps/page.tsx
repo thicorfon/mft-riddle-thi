404a3a812d35161a3179eeb8f1d49a36455a3b53
Thiago Fonseca <thicorfon@gmail.com>
2025-01-18 13:50:30 -0300

    fix image extension

--- app/o-hash-fica-pra-dps/page.tsx
+++ app/o-hash-fica-pra-dps/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <Form
       title="Nível 3 (Maria Fernanda): Apelidos"
-      image={{ src: "/flor.png", alt: "Não é prima", width: 1920, height: 960 }}
+      image={{ src: "/flor.jpg", alt: "Não é prima", width: 1920, height: 960 }}
       question="A Maria Fernanda possui muitos nomes, qual o mais conhecido?"
       inputMaxLength={8}
       hint="nome1_nome2"
