7fc97de4967824ac77c127f9dd7502f0e0f1136d
Thiago Fonseca <thicorfon@gmail.com>
2025-01-19 22:37:03 -0300

    change file name

--- app/nivel-7/page.tsx
+++ app/nivel-7/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
   return (
     <Form
       title="Nível 7: Nossa miga, você viu esse stories?!?!?!?!"
-      image={{ src: "/anchieta-imigrantes.jpg", alt: "reveillon_2122", width: 520, height: 520 }}
+      image={{ src: "/anchieta-imigrantes.jpeg", alt: "reveillon_2122", width: 520, height: 520 }}
       question="Três padrinhos/madrinhas no carro"
       inputMaxLength={14}
       hint="nome_da_banda"
